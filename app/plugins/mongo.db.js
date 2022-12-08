c22b1164d29f3dac6f53ab6c85c5d704bf6c600b
John Tomaselli <jrt@gtz.com>
2022-12-07 20:38:54 -0500

    Update mongo.db.js

--- app/plugins/mongo.db.js
+++ app/plugins/mongo.db.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 
 module.exports = fp(async (fastify, opts) => {
   console.log('Registering databases...');
-  fastify.register(require('@fastify/mongodb'), {  useUnifiedTopology: true,useNewUrlParser: true,url: process.env.MONGODB_URLbrm, name: 'brm' } )
+  fastify.register(require('@fastify/mongodb'), {  useUnifiedTopology: true,useNewUrlParser: true,url: process.env.MONGODB_URLbrm, name: 'todo' } )
   fastify.log.info('/pi 002===plugin================fastify mongodb======');
 
 });
